c5ecdb4217732c2ce213f91afc3d31895aff074b
liuhai <liuhai116163@163.com>
2019-08-23 18:32:42 +0800

    添加健康检查

--- service-health/src/main/java/com/blue/eyes/HealthApplication.java
+++ service-health/src/main/java/com/blue/eyes/HealthApplication.java
@@ -1,8 +1,9 @@
 package com.blue.eyes;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /***
  *                    .::::. 
@@ -25,7 +26,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  *                    '.:::::'                    ':'````.. 
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableAdminServer
+@EnableDiscoveryClient
 public class HealthApplication {
 
     public static void main(String[] args) {
