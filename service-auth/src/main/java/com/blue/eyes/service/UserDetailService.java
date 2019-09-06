bd6efbfe6c33fa6ba9167ca12b8e5c44fd243519
liuhai <liuhai116163@163.com>
2019-09-06 17:25:02 +0800

    更新实例模块

--- service-auth/src/main/java/com/blue/eyes/service/UserDetailService.java
+++ service-auth/src/main/java/com/blue/eyes/service/UserDetailService.java
@@ -1,9 +1,11 @@
-package com.blue.eyes.service.impl;
+package com.blue.eyes.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.blue.eyes.entity.user.User;
-import com.blue.eyes.service.UService;
 import com.blue.eyes.service.user.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -12,7 +14,7 @@ import javax.annotation.Resource;
  *                    .::::. 
  *                  .::::::::. 
  *                 :::::::::::        @author liuhai
- *             ..:::::::::::'         @date 2019-09-03 16:46
+ *             ..:::::::::::'         @date 2019-09-06 16:48
  *           '::::::::::::'           @description
  *             .:::::::::: 
  *        '::::::::::::::.. 
@@ -29,16 +31,18 @@ import javax.annotation.Resource;
  *                    '.:::::'                    ':'````.. 
  */
 @Service
-public class UserServiceImpl implements UService {
+public class UserDetailService implements UserDetailsService {
 
     @Resource
     private UserService userService;
 
     @Override
-    public User loadUserByUsername(String username) {
-        QueryWrapper<User> querWapper = new QueryWrapper<>();
-        querWapper.eq("user_name", username);
-        querWapper.eq("user_status", "1");
-        return userService.getOne(querWapper);
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        QueryWrapper<User> querWrapper = new QueryWrapper<>();
+        querWrapper.eq("user_status", "1");
+        querWrapper.eq("user_name", userName);
+        User user = userService.getOne(querWrapper);
+        return null;
     }
+
 }
