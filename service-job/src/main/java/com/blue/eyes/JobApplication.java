b92b3b7eaa347a9bfebfb32bd78ac0406d0959d4
liuhai <liuhai116163@163.com>
2019-11-08 17:10:45 +0800

    添加service-job模块

---
+++ service-job/src/main/java/com/blue/eyes/JobApplication.java
@@ -0,0 +1,36 @@
+package com.blue.eyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/***
+ *                    .::::. 
+ *                  .::::::::. 
+ *                 :::::::::::        @author liuhai
+ *             ..:::::::::::'         @date 2019-11-08 15:06
+ *           '::::::::::::'           @description
+ *             .:::::::::: 
+ *        '::::::::::::::.. 
+ *             ..::::::::::::. 
+ *           ``:::::::::::::::: 
+ *            ::::``:::::::::'        .:::. 
+ *           ::::'   ':::::'       .::::::::. 
+ *         .::::'      ::::     .:::::::'::::. 
+ *        .:::'       :::::  .:::::::::' ':::::. 
+ *       .::'        :::::.:::::::::'      ':::::. 
+ *      .::'         ::::::::::::::'         ``::::. 
+ *  ...:::           ::::::::::::'              ``::. 
+ * ```` ':.          ':::::::::'                  ::::.. 
+ *                    '.:::::'                    ':'````.. 
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+    }
+}
---
+++ service-job/src/main/java/com/blue/eyes/controller/QuartzTest.java
@@ -0,0 +1,37 @@
+package com.blue.eyes.controller;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+/***
+ *                    .::::. 
+ *                  .::::::::. 
+ *                 :::::::::::        @author liuhai
+ *             ..:::::::::::'         @date 2019-11-08 15:46
+ *           '::::::::::::'           @description
+ *             .:::::::::: 
+ *        '::::::::::::::.. 
+ *             ..::::::::::::. 
+ *           ``:::::::::::::::: 
+ *            ::::``:::::::::'        .:::. 
+ *           ::::'   ':::::'       .::::::::. 
+ *         .::::'      ::::     .:::::::'::::. 
+ *        .:::'       :::::  .:::::::::' ':::::. 
+ *       .::'        :::::.:::::::::'      ':::::. 
+ *      .::'         ::::::::::::::'         ``::::. 
+ *  ...:::           ::::::::::::'              ``::. 
+ * ```` ':.          ':::::::::'                  ::::.. 
+ *                    '.:::::'                    ':'````.. 
+ */
+public class QuartzTest extends QuartzJobBean {
+
+    private final Logger logger = LoggerFactory.getLogger(QuartzTest.class);
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        logger.info("QuartzTest Excecute {} ", jobExecutionContext.getFireInstanceId());
+    }
+}
