0beefb58e44725b7103b7c4ccdfe15ebc8f2b91a
liuhai <liuhai116163@163.com>
2019-11-14 16:30:02 +0800

    service-gateway修改

--- service-gateway/src/main/java/com/blue/eyes/config/GatewayRoutes.java
+++ service-gateway/src/main/java/com/blue/eyes/config/GatewayRoutes.java
@@ -1,14 +1,15 @@
-package com.blue.eyes;
+package com.blue.eyes.config;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /***
  *                    .::::. 
  *                  .::::::::. 
  *                 :::::::::::        @author liuhai
- *             ..:::::::::::'         @date 2019-08-21 10:32
+ *             ..:::::::::::'         @date 2019-11-14 14:02
  *           '::::::::::::'           @description
  *             .:::::::::: 
  *        '::::::::::::::.. 
@@ -24,12 +25,14 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * ```` ':.          ':::::::::'                  ::::.. 
  *                    '.:::::'                    ':'````.. 
  */
-@SpringBootApplication
-@EnableDiscoveryClient
-public class GatewayApplication {
+@Configuration
+public class GatewayRoutes {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                /*.route("route_mail", r -> r.path("/hello")
+                        .uri("http://localhost:8081/"))*/
+                .build();
     }
-
 }
